915f9b1fa06b0e7fa6256c09af4d73b49813dcf3
ksf <ksf@localhost.localdomain>
2023-11-26 12:45:42 -0300

    delete vote and switch vote

--- src/main/java/com/kelton/clonnit/service/VoteService.java
+++ src/main/java/com/kelton/clonnit/service/VoteService.java
@@ -25,18 +25,51 @@ public class VoteService {
     public void vote(VoteDto voteDto) {
         final Post post = postRepository.findById(voteDto.getPostId())
                 .orElseThrow(() -> new ClonnitException("Post not found"));
-        Optional<Vote> voteByPostAndUser = voteRepository.findTopByPostAndClonnitorOrderByIdDesc(post, authService.getCurrentUser());
+        Optional<Vote> voteByPostAndUser = voteRepository
+                .findTopByPostAndClonnitorOrderByIdDesc(post, authService.getCurrentUser());
 
-        if(voteByPostAndUser.isPresent() && voteByPostAndUser.get().getVoteType().equals(voteDto.getVoteType())) {
-            throw new ClonnitException("You have voted already");
+        if (voteByPostAndUser.isPresent()) {
+            if (isTheSameVoteType(voteByPostAndUser.get(), voteDto)) {
+                cancelVote(post, voteDto, voteByPostAndUser.get());
+            } else {
+                switchVote(post, voteDto, voteByPostAndUser.get());
+            }
+            return;
         }
-        if(VoteType.UPVOTE.equals(voteDto.getVoteType())) {
+
+        if (VoteType.UPVOTE.equals(voteDto.getVoteType())) {
             post.setVoteCount(post.getVoteCount() + 1);
         } else {
             post.setVoteCount(post.getVoteCount() - 1);
         }
 
         voteRepository.save(mapToVote(voteDto, post));
+        postRepository.save(post);
+    }
+
+    private boolean isTheSameVoteType(Vote voteByPostAndUser, VoteDto voteDto) {
+        return voteByPostAndUser.getVoteType().equals(voteDto.getVoteType());
+    }
+
+    private void cancelVote(Post post, VoteDto voteDto, Vote vote) {
+        if (VoteType.UPVOTE.equals(voteDto.getVoteType())) {
+            post.setVoteCount(post.getVoteCount() - 1);
+        } else {
+            post.setVoteCount(post.getVoteCount() + 1);
+        }
+        this.voteRepository.deleteById(vote.getId());
+        this.postRepository.save(post);
+    }
+
+    private void switchVote(Post post, VoteDto voteDto, Vote vote) {
+        if (VoteType.UPVOTE.equals(voteDto.getVoteType())) {
+            post.setVoteCount(post.getVoteCount() + 2);
+            vote.setVoteType(VoteType.UPVOTE);
+        } else {
+            post.setVoteCount(post.getVoteCount() - 2);
+            vote.setVoteType(VoteType.DOWNVOTE);
+        }
+        voteRepository.save(vote);
         postRepository.save(post);
     }
 
